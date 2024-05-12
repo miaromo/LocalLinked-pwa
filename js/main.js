824470989dd0501e4bff7ad82e1ffa1570661ab7
miaromo <164634156+miaromo@users.noreply.github.com>
2024-05-12 19:42:13 +0100

    big-update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-
 function changePage(page) {
     console.log(page);
-    circle = document.createElement("img");
-    circle.id = "circle"; circle.src = "img/loadcircle.png";
-    document.body.insertBefore(circle,document.body.childNodes[0]);
+    animateCircle();
+    
     var title = document.getElementById("title");
+
     var newtitle = page.slice(0,-5);
     newtitle = toTitleCase(newtitle);
     title.innerHTML = newtitle;
     newtitle = "LocalLinked - " + newtitle;
     document.title = newtitle;
+    
     var elements = document.querySelectorAll('.page');
     elements = [...elements];
     elements.forEach( div => {
@@ -17,44 +17,49 @@ function changePage(page) {
             div.classList.remove("active");
         }
     })
+    
     console.log(elements);
+    
     var active = document.getElementById(page);
-    circle = document.getElementById("circle");
     active.classList.add("active");
+}
+
+function toTitleCase(word) {
+    return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+}
+
+function animateCircle(){
+    circle = document.createElement("img");
+    circle.id = "circle"; circle.src = "img/loadcircle.png";
+    document.body.insertBefore(circle,document.body.childNodes[0]);
+    circle = document.getElementById("circle");
     circle.classList.add('animate');
     $("#circle").css({
         left:`${xPercent}px`,
         top:`100%`,
     });
-    window.setTimeout( stopAnimate, 2000 );
+    setTimeout(findAnims, 2000);
 }
 
-function toTitleCase(word) {
-    return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
- }
+function findAnims(){
+    var elements = document.getElementsByClassName("animate");
+    elearray = Array.from(elements)
+    elearray.forEach(stopCircle);
+}
 
- function stopAnimate() {
-    if(document.getElementById("circle") != null){
-        var element = document.getElementById("circle");
+function stopCircle(){
+    element = document.getElementById("circle");
+    if(element != null){
         var scaleX = element.getBoundingClientRect().width / element.offsetWidth;
-        if(scaleX > 70) {
+        console.log(scaleX);
+        if(scaleX > 70){
             element.parentNode.removeChild(element);
         }
-        ani = document.getElementsByClassName("animate");
     }
-    while (true){
-        stopAnimate();
-    }
-    // while(ani[0]){
-    //     var element = document.getElementById("circle");
-    //     var scaleX = element.getBoundingClientRect().width / element.offsetWidth;
-    //     console.log(scaleX);
-    //     if(scaleX > 1500) {
-    //         ani[0].parentNode.removeChild(ani[0]);
-    //     }
-    // }
- }
+}
 
- $(document).mousemove(function(e) {
+$(document).mousemove(function(e) {
     xPercent = e.pageX;
-  });
+});
+
+setInterval(findAnims, 100);
